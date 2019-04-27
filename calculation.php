d50b2e0aad8b3ae16f7e4198d5da088a6cdf67db
janadari <31371187+janadari@users.noreply.github.com>
2019-04-27 14:54:18 +0530

    1st step

---
+++ calculation.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+    <style type="text/css">
+        body{
+            
+        background-color: #e6fd84;
+        }
+        
+        </style>
+    </head>
+    <body >
+<?php
+
+$a="$_POST[a]";
+$b="$_POST[b]";
+$c="$_POST[c]";
+
+
+
+echo  "<h1>$a x^2 + $b x + $c = 0<br></h1>";
+
+$SQ = sqrt(($b * $b)-(4 * $a * $c));
+#echo $SQ;
+
+if($SQ > "0"){
+echo "<br><h2>x  =  ",(( -$b + $SQ)/ (2 * $a)),"<br>";
+
+echo "    &    <br>";
+ echo "x =  ",(( - $b - $SQ)/ (2 * $a));
+    
+} else
+       echo "no solutions";
+
+?>
+   </body> </html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head><style type ="text/css">
+    
+    .title{
+       font-family: cursive;
+        : 
+    }
+    body{
+        background-color: #f7d7d7;
+    }
+    
+    .yellow{
+        background-color: yellow;
+        color: red;
+    font-family : serif;
+        font-family: cursive;
+        font-size: 20;
+        font-style: oblique;
+    }
+    </style>
+    
+    </head>
+    
+    <body>
+        <h1 class="title">Quadratic Equation Solver</h1>
+        <form action="calculation.php" method="post">
+        <table><tr>
+            <th><h1>a</h1></th><th><h1>b</h1></th><th><h1>c</h1></th></tr>
+            <tr><td><input type="text" name="a">  x^2  +     </td>
+            <td><input type="text" name="b">  x  +  </td>
+            <td><input type="text" name="c"></td>
+             <td>  =  0</td></tr>
+        <br>
+            <tr><td></td><td><br><br><input type="submit" class="yellow" value= "FIND THE SOLUTIONS"></td></tr>
+            </table>
+            
+           
+        </form>
+    
+    </body>
+</html>
